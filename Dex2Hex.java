4a4798f0da0502e588c2147c1e8736c756ca60a4
lander302 <lander302@caledonian.ac.uk>
2024-11-05 19:25:12 +0000

    changes

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 class Dex2Hex
 {
-public static int Arg1;
+
+
+    public static int Arg1;
     public static void main(String args[])    {
+
+	if (args.length == 0) {
+            System.out.println("Error: No input argument provided.");
+            return;
+        }
+        
+        try {
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Invalid input. Please provide an integer.");
+            return;
+        }
+
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
@@ -23,4 +38,4 @@ public static int Arg1;
         System.out.println("The hexadecimal representation is: " + hexadecimal);
 
     }
-}
+}	

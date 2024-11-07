08e00eb18f07b2fa5916d6fe411e56cf00040af4
lander302 <lander302@caledonian.ac.uk>
2024-11-07 20:44:11 +0000

    changed brackets

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -20,7 +20,7 @@ class Dex2Hex {
         }
 
 
-        char[] ch ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char ch[] ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem; 
 	num = arg1;
         StringBuilder hexadecimal = new StringBuilder();

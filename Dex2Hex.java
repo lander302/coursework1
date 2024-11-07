615e28489b636dade06f4e03f25c68bb63d12eed
lander302 <lander302@caledonian.ac.uk>
2024-11-07 16:20:23 +0000

    Fixed errors again

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -19,10 +19,10 @@ class Dex2Hex {
         }
 
 
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E',>
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E', 'F'};
         int rem, num = arg1;
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex....>
+        System.out.println("Converting the Decimal Value " + num + " to Hex....">);
 
 
 

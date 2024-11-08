c76e4dc25da4bec1216550299dca45a1af05cba8
lander302 <lander302@caledonian.ac.uk>
2024-11-08 11:14:27 +0000

    checking test

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -7,7 +7,7 @@ class Dex2Hex {
 
     public static void main(String args[])    {
         if (args.length == 0) {
-            System.out.println("Error: \n No input given");
+            System.out.println("2Error: \n No input given");
             return;
         }
         int arg1;

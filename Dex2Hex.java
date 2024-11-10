04075da0cd3f56123d4932deded0d2b59180fc0a
lander302 <lander302@caledonian.ac.uk>
2024-11-10 22:16:20 +0000

    deleted comments

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -9,27 +9,8 @@ import java.util.logging.Handler;
 import java.io.IOException;
 
 class Dex2Hex {
-   // public static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
-
+  
     public static void main(String args[])    {
-
-        // Create a ByteArrayOutputStream to capture log output
-//        ByteArrayOutputStream logStream = new ByteArrayOutputStream();
-  //      Handler logHandler = new ConsoleHandler() {
-    //        @Override
-     //        public void publish(java.util.logging.LogRecord record) {
-       //         try {
-                    // Write the formatted log message to logStream
-         //           logStream.write((getFormatter().format(record)).getBytes());
-           //     } catch (IOException e) {
-                    // Handle the IOException (you can log it if necessary)
-             //       logger.severe("Error writing to log stream: " + e.getMessage());
-               // }
-           // }
-       // };        
-//	logHandler.setLevel(Level.ALL);
-//	logger.addHandler(logHandler);
-
 
 	if (args.length == 0) {
             System.out.println("Error: No input has been given");

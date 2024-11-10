5f5f50d401db6f0b35e37e153f16137891b191fb
lander302 <lander302@caledonian.ac.uk>
2024-11-10 16:51:54 +0000

    back to mine

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,26 +1,44 @@
-
 import java.util.Scanner;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import java.util.logging.Logger;
 
-class Dex2Hex
-{
-public static int Arg1;
+
+class Dex2Hex {
+    public static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
+
     public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+        if (args.length == 0) {
+            logger.info("Error: \n No input given");
+            return;
         }
+        int arg1;
+        try {
+            arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            logger.info("Error: Please input an integer.");
+            return;
+  }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
 
-    }
+        char[] ch ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem;
+        int num = arg1;
+        StringBuilder hexadecimal = new StringBuilder();
+
+
+        logger.info("Converting the Decimal Value " + num + " to Hex...");
+
+
+        while(num != 0){
+            rem = num % 16;
+            hexadecimal.insert(0, ch[rem]);
+            num = num / 16;
+        }
+		
+		
+        logger.info("Hexadecimal representation is: " + hexadecimal);
+        logger.info("Your integer has been converted");
+}
 }
 

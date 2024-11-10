38ccf745d8bad6c61e624732e4d64b8553d65c44
lander302 <lander302@caledonian.ac.uk>
2024-11-10 21:23:16 +0000

    Exception

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.fail;
 import java.util.logging.Logger;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
+import java.io.ByteArrayOutputStream;
+import java.util.logging.Handler;
+import java.io.IOException;
 
 class Dex2Hex {
     public static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
@@ -14,13 +17,16 @@ class Dex2Hex {
         ByteArrayOutputStream logStream = new ByteArrayOutputStream();
         Handler logHandler = new ConsoleHandler() {
             @Override
-            public void publish(java.util.logging.LogRecord record) {
-                logStream.write((getFormatter().format(record)).getBytes());
+             public void publish(java.util.logging.LogRecord record) {
+                try {
+                    // Write the formatted log message to logStream
+                    logStream.write((getFormatter().format(record)).getBytes());
+                } catch (IOException e) {
+                    // Handle the IOException (you can log it if necessary)
+                    logger.severe("Error writing to log stream: " + e.getMessage());
+                }
             }
-        };
-
-
-        
+        };        
 	logHandler.setLevel(Level.ALL);
 	logger.addHandler(logHandler);
 

837234288348b7246a4938b5234213ba5ae7c960
lander302 <lander302@caledonian.ac.uk>
2024-11-10 22:09:17 +0000

    chnages

--- Dex2HexTest.java
+++ Dex2HexTest.java
@@ -2,6 +2,7 @@ import org.junit.*;
 import static org.junit.Assert.assertTrue;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import static org.junit.Assert.asserEquals;
 import java.util.logging.*;
 
 public class Dex2HexTest {

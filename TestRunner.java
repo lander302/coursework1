ec07cf77d5f5f46d4a6dbb8bf0ce9fed6d3b3afe
lander302 <lander302@caledonian.ac.uk>
2024-11-11 10:45:25 +0000

    checking

--- TestRunner.java
+++ TestRunner.java
@@ -7,11 +7,14 @@ import java.util.logging.Logger;
 
 public class TestRunner {
 
+//Initialises Logger
 private static final Logger logger = Logger.getLogger(TestRunner.class.getName());
 
 	public static void main(String[] args) {
       		Result result = JUnitCore.runClasses(Dex2HexTest.class);
       		int fails = 0;
+
+	//Uses try catch method to check is tests were successful and then outputs repsonse 
       		try{
           		for (Failure failure : result.getFailures()) {
              			logger.info(failure.toString());
